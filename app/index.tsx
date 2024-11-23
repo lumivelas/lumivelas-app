58c8112a013965dfafdfdb89e15ef569152b5353
Luis Henrique <luishenriquealbino@gmail.com>
2024-11-23 13:11:30 -0300

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="h-full w-full items-center justify-center select-none">
+      <Text>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
