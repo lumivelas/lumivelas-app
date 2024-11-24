cae4009d068af7afca44b1fec7da4073eabb2a09
Luis Henrique <luishenriquealbino@gmail.com>
2024-11-24 09:43:49 -0300

    Configurado fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
 import "../global.css";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) return null;
+
   return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    colors: {
+      white: '#EAE4D3',
+      danger: 'B35442',
+      primary: '#5A7D3A',
+      secundary: '#C27A4A',
+    },
+    extend: {
+      fontFamily: {
+        bold: [ 'Poppins-Bold' ],
+        thin: [ 'Poppins-Thin' ],
+        regular: [ 'Poppins-Bold' ]
+      }
+    },
   },
   plugins: [],
 }
