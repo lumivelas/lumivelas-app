ad27e865200352c12d7b92b38b82710e0d8b33d1
Luis Henrique <luishenriquealbino@gmail.com>
2024-11-26 21:36:50 -0300

    Finalizando botão customizado

--- src/components/LumiButton.tsx
+++ src/components/LumiButton.tsx
@@ -1,16 +1,30 @@
 'use client';
 
-import { Button } from '@mui/material'
-import React from 'react'
+import { Button, ButtonProps } from '@mui/material';
+import React from 'react';
 
-interface Props {
+interface Props extends ButtonProps {
   title: string;
 }
 
-const LumiButton : React.FC<Props> = ({ title }) => {
+const LumiButton: React.FC<Props> = ({ title, color = "primary" }) => {
   return (
-    <Button color="secondary" variant="contained">{ title }</Button>
-  )
-}
+    <Button
+      sx={{
+        textTransform: "none",
+        fontWeight: "bold",
+        borderWidth: 2,
+        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+        "&:hover": {
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+        },
+      }}
+      color={color}
+      variant="outlined"
+    >
+      {title}
+    </Button>
+  );
+};
 
 export default LumiButton;
