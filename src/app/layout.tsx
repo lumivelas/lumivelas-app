d78e8f9afd99e9c6c596a551c22f4b80261f695e
Luis Henrique <luishenriquealbino@gmail.com>
2024-11-26 21:05:06 -0300

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,7 @@ const poppins = Poppins({
   weight: "400",
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children } : Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={poppins.className}>
